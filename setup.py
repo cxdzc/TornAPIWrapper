3f5e94d85c46ed3c525e71e2c50f81cbcd6ca882
cxdcz <110936008+cxdzc@users.noreply.github.com>
2023-12-12 22:53:31 -0800

    A commit and not a comet.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    author='cxdzc',
+    description='A Python wrapper for the Torn City API, providing access to Torn City data.',
+    install_requires=['requests'],
+    license='MIT License',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    name='TornAPIWrapper',
+    packages=['torn_api_error_handler', 'torn_api_wrapper'],
+    url='https://github.com/cxdzc/TornAPIWrapper',
+    version='v1.0.0',
+)
