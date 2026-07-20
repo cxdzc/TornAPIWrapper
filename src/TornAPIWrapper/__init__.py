2ab0aa94d0933c0c27b330ef3ed5d22c86115cdf
cxdzc <110936008+cxdzc@users.noreply.github.com>
2026-07-20 01:44:33 -0700

    - fix versioning.

--- src/TornAPIWrapper/__init__.py
+++ src/TornAPIWrapper/__init__.py
@@ -26,7 +26,7 @@ __author__ = "cxdzc"
 __copyright__ = "Copyright 2023-Present cxdzc"
 __license__ = "MIT"
 __title__ = "TornAPIWrapper"
-__version__ = "2.10.0"
+__version__ = "2.10.1"
 __patch__ = "16.07.2026"
 
 from .errors import (
