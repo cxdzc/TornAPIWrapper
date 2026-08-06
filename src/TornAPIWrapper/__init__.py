7fe66df7d7d2eb930f42cafed1d5f7c0101ed447
cxdzc <110936008+cxdzc@users.noreply.github.com>
2026-08-06 16:36:48 -0700

    Market.py - Get points market listings.
    User.py
    - Get your networth.
    - Get your current perks.
    - 2 Columns removed from user.py in the get_snapshot selection.

--- src/TornAPIWrapper/__init__.py
+++ src/TornAPIWrapper/__init__.py
@@ -26,8 +26,8 @@ __author__ = "cxdzc"
 __copyright__ = "Copyright 2023-Present cxdzc"
 __license__ = "MIT"
 __title__ = "TornAPIWrapper"
-__version__ = "2.11.0"
-__patch__ = "27.07.2026"
+__version__ = "2.12.0"
+__patch__ = "06.08.2026"
 
 from .errors import (
     WrapperError,
